783bdac58bc99f6de5f580da4eadf9e9f6f260d0
jps <milaperipolli@gmail.com>
2018-05-28 11:10:14 -0300

    Eureka server config

--- src/main/java/com/jps/example/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/jps/example/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.jps.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
