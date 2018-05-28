8af4b3dc8c9b4e76dc9a4b4c64ac87b36159f51f
jps <milaperipolli@gmail.com>
2018-05-28 11:09:25 -0300

    First commit

---
+++ src/main/java/com/jps/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.jps.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
